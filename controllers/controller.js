2a2dbba8bea7951579569c83f5b4a8fa77641615
Citlalli Prado <citlalli.prado@gmail.com>
2019-05-15 22:28:58 +0200

    Fase 1 corrected - creates 3 thrusters, functions create and show separated, hide create button once clicked

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,31 @@
 "use strict";
-function createRockets(rocket1, rocket2, thrusterRocket1, thrusterRocket2) {
+/**
+ * no pasar parametros !!!!
+separar funciones
+testear antes de crear objeto
+ocultar lo que no se necesita
+ */
+function createRockets() {
+    var rocket1, rocket2;
+    rocket1 = ['32WESSDS', 3];
+    rocket2 = ['LDSFJA32', 6];
+    var rockets = [rocket1, rocket2];
+    var myRockets = [];
+    for (var i = 0; i < rockets.length; ++i) {
+        myRockets[i] = new Rocket(rockets[i][0]);
+        for (var e = 0; e < rockets[i][1]; e++) {
+            myRockets[i].addThruster(0);
+        }
+    }
+    showRockets(myRockets);
+}
+function showRockets(myRockets) {
+    var button = document.querySelector('#createButton');
+    button.classList.add("d-none");
     var displayRocketsContainer = document.querySelector('.displayRockets');
     displayRocketsContainer.textContent = "";
-    var rocketsToCreate = [rocket1, rocket2];
-    var thrustersToCreate = [thrusterRocket1, thrusterRocket2];
-    var myRockets = [];
-    for (var i = 0; i < rocketsToCreate.length; ++i) {
-        myRockets[i] = new Rocket(rocketsToCreate[i]);
-        myRockets[i].addThruster(thrustersToCreate[i]);
+    for (var i = 0; i < myRockets.length; ++i) {
         var rocket = document.createElement('p');
-        displayRocketsContainer.appendChild(rocket).textContent = "Rocket " + myRockets[i].code + " has " + myRockets[i].thrusters + " thrusters";
+        displayRocketsContainer.appendChild(rocket).textContent = "Rocket " + myRockets[i].code + " has " + myRockets[i].thrusters.length + " thrusters";
     }
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,15 +1,36 @@
-function createRockets(rocket1:string, rocket2:string, thrusterRocket1:number, thrusterRocket2:number) {    
-    
+/**
+ * no pasar parametros !!!!
+separar funciones
+testear antes de crear objeto
+ocultar lo que no se necesita
+ */
+
+function createRockets() {    
+    let rocket1:any, rocket2:any
+    rocket1=['32WESSDS',3]
+    rocket2=['LDSFJA32',6]
+    let rockets = [rocket1,rocket2]
+    let myRockets:any = []
+
+    for (let i = 0; i < rockets.length; ++i) {
+        myRockets[i] = new Rocket(rockets[i][0]);
+        for (let e = 0; e < rockets[i][1]; e++) {
+            myRockets[i].addThruster(0);    
+        }
+                  
+    }
+   
+    showRockets(myRockets);
+}
+
+function showRockets(myRockets:any){
+    let button:any = document.querySelector('#createButton')! 
+    button.classList.add("d-none");
     let displayRocketsContainer:any = document.querySelector('.displayRockets')! 
     displayRocketsContainer.textContent = "";
-    let rocketsToCreate:any = [rocket1,rocket2]
-    let thrustersToCreate:any = [thrusterRocket1,thrusterRocket2]
-    let myRockets = []
-    for (let i = 0; i < rocketsToCreate.length; ++i) {
-        myRockets[i] = new Rocket(rocketsToCreate[i]);
-        myRockets[i].addThruster(thrustersToCreate[i]);
-     
+
+    for (let i = 0; i < myRockets.length; ++i) {
         let rocket = document.createElement('p'); 
-        displayRocketsContainer!.appendChild(rocket).textContent = `Rocket ${myRockets[i].code} has ${myRockets[i].thrusters} thrusters`
+        displayRocketsContainer!.appendChild(rocket).textContent = `Rocket ${myRockets[i].code} has ${myRockets[i].thrusters.length} thrusters`
     }
 }
