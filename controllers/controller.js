0500b142282d6d9f86da75cf92d55a41223009ab
citlas <citlalli.prado@gmail.com>
2019-05-15 10:09:36 +0200

    Fase 1 - done. Shows rockets and thrusters

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hello');
+function createRockets(rocket1, rocket2, thrusterRocket1, thrusterRocket2) {
+    var displayRocketsContainer = document.querySelector('.displayRockets');
+    displayRocketsContainer.textContent = "";
+    var rocketsToCreate = [rocket1, rocket2];
+    var thrustersToCreate = [thrusterRocket1, thrusterRocket2];
+    var myRockets = [];
+    for (var i = 0; i < rocketsToCreate.length; ++i) {
+        myRockets[i] = new Rocket(rocketsToCreate[i]);
+        myRockets[i].addThruster(thrustersToCreate[i]);
+        var rocket = document.createElement('p');
+        displayRocketsContainer.appendChild(rocket).textContent = "Rocket " + myRockets[i].code + " has " + myRockets[i].thrusters + " thrusters";
+    }
+}
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+function createRockets(rocket1:string, rocket2:string, thrusterRocket1:number, thrusterRocket2:number) {    
+    
+    let displayRocketsContainer:any = document.querySelector('.displayRockets')! 
+    displayRocketsContainer.textContent = "";
+    let rocketsToCreate:any = [rocket1,rocket2]
+    let thrustersToCreate:any = [thrusterRocket1,thrusterRocket2]
+    let myRockets = []
+    for (let i = 0; i < rocketsToCreate.length; ++i) {
+        myRockets[i] = new Rocket(rocketsToCreate[i]);
+        myRockets[i].addThruster(thrustersToCreate[i]);
+     
+        let rocket = document.createElement('p'); 
+        displayRocketsContainer!.appendChild(rocket).textContent = `Rocket ${myRockets[i].code} has ${myRockets[i].thrusters} thrusters`
+    }
+}
